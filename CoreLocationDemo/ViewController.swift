a9ddc3fe03f208966340a4066a7c7d326d3fb707
Chris Rabl <rabl.christopher@gmail.com>
2015-01-17 16:17:18 -0700

    Initial Commit

---
+++ CoreLocationDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreLocationDemo
+//
+//  Created by Chris Rabl on 2015-01-17.
+//  Copyright (c) 2015 Coulee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
