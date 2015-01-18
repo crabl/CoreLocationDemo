7f27672ea3df43e230054d95163f7d4f3c2b826d
Chris Rabl <rabl.christopher@gmail.com>
2015-01-17 17:18:10 -0700

    Location logging is enabled

--- CoreLocationDemo/AppDelegate.swift
+++ CoreLocationDemo/AppDelegate.swift
@@ -8,10 +8,9 @@
 
 import UIKit
 import CoreData
-import CoreLocation
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
@@ -20,22 +19,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
         return true
     }
     
-    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
-        //
-    }
-    
-    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
-        for location in locations {
-            self.locations.append(location as CLLocation)
-        }
-        
-        println(self.locations)
-    }
-    
-    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
-        //
-    }
-
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
--- CoreLocationDemo/ViewController.swift
+++ CoreLocationDemo/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreLocation
 import MapKit
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
     var locationManager = CLLocationManager()
     var locations: [CLLocation] = []
@@ -21,6 +21,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         initLocationManager()
+        initMapView()
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,6 +35,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.requestAlwaysAuthorization()
         locationManager.startUpdatingLocation()
     }
+    
+    func initMapView() {
+        mapView.delegate = self
+        mapView.mapType = .Standard
+        mapView.showsUserLocation = true
+    }
 
     func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
         //
@@ -45,11 +52,36 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             self.locations.append(location as CLLocation)
         }
         
-        println(self.locations[self.locations.count - 1])
+        let spanX = 0.007
+        let spanY = 0.007
+        var newRegion = MKCoordinateRegion(center: mapView.userLocation.coordinate, span: MKCoordinateSpanMake(spanX, spanY))
+        mapView.setRegion(newRegion, animated: true)
+        
+        if (self.locations.count > 1) {
+            var sourceIndex = self.locations.count - 1
+            var destinationIndex = self.locations.count - 2
+            
+            let c1 = self.locations[sourceIndex].coordinate
+            let c2 = self.locations[destinationIndex].coordinate
+            var a = [c1, c2]
+            var polyline = MKPolyline(coordinates: &a, count: a.count)
+            mapView.addOverlay(polyline)
+        }
     }
     
     func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
         //
+    }
+    
+    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+        
+        if overlay is MKPolyline {
+            var polylineRenderer = MKPolylineRenderer(overlay: overlay)
+            polylineRenderer.strokeColor = UIColor.blueColor()
+            polylineRenderer.lineWidth = 4
+            return polylineRenderer
+        }
+        return nil
     }
 }
 
